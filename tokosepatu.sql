dcd67fc562e4d09851f3876936b257b4dbcefbf2
Putro Dwi <putrodwi31@gmail.com>
2023-01-11 22:54:02 +0700

    Update database

--- tokosepatu.sql
+++ tokosepatu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 01 Jan 2023 pada 15.50
+-- Waktu pembuatan: 11 Jan 2023 pada 16.52
 -- Versi server: 10.4.22-MariaDB
 -- Versi PHP: 8.1.12
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `tokosepatu`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id` int(11) NOT NULL,
+  `nama_kategori` varchar(50) NOT NULL,
+  `img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id`, `nama_kategori`, `img`) VALUES
+(1, 'Sneakers', 'sneakers.jpg'),
+(2, 'Slip On', 'shoes-g2e3384b48_1920.jpg'),
+(3, 'Boots', '');
 
 -- --------------------------------------------------------
 
@@ -49,12 +70,21 @@ INSERT INTO `role` (`id_role`, `namarole`) VALUES
 CREATE TABLE `sepatu` (
   `kode_sepatu` varchar(15) NOT NULL,
   `nama_sepatu` varchar(50) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
   `merk` varchar(30) NOT NULL,
   `ukuran` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
   `stok` int(11) NOT NULL,
-  `keterangan` text NOT NULL
+  `keterangan` text NOT NULL,
+  `foto` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `sepatu`
+--
+
+INSERT INTO `sepatu` (`kode_sepatu`, `nama_sepatu`, `id_kategori`, `merk`, `ukuran`, `harga`, `stok`, `keterangan`, `foto`) VALUES
+('S0001', 'Nike Air Jordan', 2, 'Nike', 40, 8000000, 100, 'Tersedia', 'nike.png');
 
 -- --------------------------------------------------------
 
@@ -84,6 +114,7 @@ CREATE TABLE `user` (
   `nama` varchar(75) NOT NULL,
   `alamat` text NOT NULL,
   `no_telp` varchar(20) NOT NULL,
+  `code` text NOT NULL,
   `tgl_lahir` date NOT NULL,
   `role_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
@@ -94,12 +125,20 @@ CREATE TABLE `user` (
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `email`, `password`, `nama`, `alamat`, `no_telp`, `tgl_lahir`, `role_id`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@admin.com', '$2y$10$hPUqEUjwmkw3al9BiK1xCem8GpV7j/y5VKK1yyMHSPRrfvyzxVSNC', 'admin', '-', '-', '2023-01-01', 1, '2023-01-01 15:03:29', '2023-01-01 15:03:29');
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `nama`, `alamat`, `no_telp`, `code`, `tgl_lahir`, `role_id`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'admin@admin.com', '$2y$10$dLp.AHnenxdfCDLWi.p6kejkZbVHsQ.A88R90HrCQSST.kSHYgc1G', 'admin', '-', '-', '', '2023-01-01', 1, '2023-01-01 15:03:29', '2023-01-01 15:03:29'),
+(5, 'ican9998', 'ihsanuddin2323@gmail.com', '$2y$10$hFnIb9hxIlSCdreedahkAu1r3zdilFTrvoXjizdKuZ3piEIoxPo/W', 'ican', '', '', 'Z3UR6DSE2L', '2023-01-13', 2, '0000-00-00 00:00:00', '2023-01-11 04:33:15'),
+(6, 'putro9935', 'putrodwi31@gmail.com', '$2y$10$3QmLERfQcjenZIhi6yUsuuz3XuNiSnCRpoxdJvEhFr5D9VLYBJUyS', 'Putro Dwi Mulyo', '', '', '', '2023-01-04', 2, '2023-01-11 04:37:58', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `role`
@@ -131,6 +170,12 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
 -- AUTO_INCREMENT untuk tabel `role`
 --
 ALTER TABLE `role`
@@ -146,7 +191,7 @@ ALTER TABLE `transaksi`
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
